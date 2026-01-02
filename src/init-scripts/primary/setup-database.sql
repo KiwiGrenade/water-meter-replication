b81225690d444a3de98b26894961d95242db1343
Mango T14 <kubajaskow@wp.pl>
2026-01-02 02:02:23 +0100

    Jebane SSH kurwa jego mać, CHUJ WAM W DUPE

--- src/init-scripts/primary/setup-database.sql
+++ src/init-scripts/primary/setup-database.sql
@@ -7,6 +7,7 @@
 --  - OPERATOR   – dodaje/edytuje odczyty
 --  - CLIENT     – klient końcowy, widzi swoje liczniki
 CREATE ROLE replicator WITH REPLICATION LOGIN PASSWORD 'replicator_password';
+CREATE ROLE failover LOGIN PASSWORD 'failover_pass' SUPERUSER;
 CREATE TYPE user_role AS ENUM ('ADMIN', 'OPERATOR', 'CLIENT');
 
 -- Opcjonalny status licznika (czy licznik jest aktywny)
